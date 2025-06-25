d2b26c9886201bf74fa7a5cd549e7e844d2e9758
Sutibro Barua <boyback485@gmail.com>
2025-06-25 15:03:37 -0400

    created two questions and const variable named questions

---
+++ index.js
@@ -0,0 +1,28 @@
+const questions = [
+
+    {
+        question: "What color is the sky daytime?",
+        answers: ["Blue","Green", "Red", "Purple"],
+        correct: "Blue"
+    },
+
+    {
+        question: "How many legs does a spider have",
+        answers: ["6", "8","10","14"],
+        correct: "8"
+    },
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+]
