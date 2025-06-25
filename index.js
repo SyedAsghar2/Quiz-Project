038d44448ef8f7224e5957ddfe8bcdc476d39d0a
Sutibro Barua <boyback485@gmail.com>
2025-06-25 14:47:34 -0400

    created question 2

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const questions = [
         answers: ["Blue", "Green", "Red", "Purple"],
         correct: "Blue"
     },
-    
+
+    {
+        question: "How many legs does a spider have?",
+        answers: ["6","8", "10","14"],
+        correct: "8"
+    },
 
 
 ]
